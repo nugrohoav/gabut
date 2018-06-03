c9edef208ef2c78b0049e23194203bd90fb35ebf
Muhammad Wafa <wafax.4@gmail.com>
2018-06-03 17:38:24 +0700

    tebak angka

---
+++ tebak_angka.py
@@ -0,0 +1,9 @@
+print("Pikirkan satu angka dari 1 sampai 31, kami akan menebaknya")
+print("=="*10)
+
+tebakan = 0
+for k in range(5):
+    i = 2**k
+    print([j for j in range(32) if (j//i)%2 == 1])
+    tebakan = tebakan + i if input("ada disini?[Y/N]") in ["y","Y"] else tebakan
+print(tebakan)
